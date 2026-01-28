d95f75c4b1a8eb937dad3bdb4edfa417b9e4d446
DinaSiagian <143679301+DinaSiagian@users.noreply.github.com>
2026-01-28 08:51:34 +0700

    perbaikan qr kode

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api'
+    // SEBELUMNYA: baseURL: 'http://localhost:8000/api'
+    baseURL: "http://10.181.7.29:8000/api", // GANTI DENGAN IP KAMU
 });
 
 export default api;
